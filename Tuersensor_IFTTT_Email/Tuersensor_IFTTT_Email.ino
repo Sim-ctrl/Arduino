85807990f35b5f07876267b8ced163651bdae4d2
Sim-ctrl <77060991+Sim-ctrl@users.noreply.github.com>
2021-05-03 16:40:10 +0200

    Add files via upload

---
+++ Tuersensor_IFTTT_Email/Tuersensor_IFTTT_Email.ino
@@ -0,0 +1,10 @@
+<NotepadPlus>
+    <Session activeView="0">
+        <mainView activeIndex="2">
+            <File firstVisibleLine="0" xOffset="0" scrollWidth="976" startPos="242" endPos="316" selMode="0" lang="Python" encoding="-1" filename="C:\Users\Simon\Desktop\Skripts\testv2.py" backupFilePath="Z:\hacken\Notepad++\backup\testv2.py@2020-11-24_175043" originalFileLastModifTimestamp="-1309183820" originalFileLastModifTimestampHigh="30851711" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="0" mapWrapIndentMode="-1" mapIsWrap="no" />
+            <File firstVisibleLine="0" xOffset="0" scrollWidth="824" startPos="321" endPos="321" selMode="0" lang="Python" encoding="-1" filename="C:\Users\Simon\Desktop\Skripts\nuroffen.py" backupFilePath="" originalFileLastModifTimestamp="-408630601" originalFileLastModifTimestampHigh="30851718" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="0" mapWrapIndentMode="-1" mapIsWrap="no" />
+            <File firstVisibleLine="4" xOffset="0" scrollWidth="1368" startPos="1313" endPos="1313" selMode="0" lang="C++" encoding="-1" filename="J:\Arduino\Tuersensor_IFTTT_Email\Tuersensor_IFTTT_Email.ino" backupFilePath="Z:\hacken\Notepad++\backup\Tuersensor_IFTTT_Email.ino@2021-05-03_163927" originalFileLastModifTimestamp="1854707200" originalFileLastModifTimestampHigh="30883675" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="5374031" mapWrapIndentMode="-1" mapIsWrap="no" />
+        </mainView>
+        <subView activeIndex="0" />
+    </Session>
+</NotepadPlus>
